f22372db44d2c2e014a29fde550d7c6d7a03d9f5
liliyuan <782821705@qq.com>
2020-06-01 16:19:48 +0800

    OCR认证用户功能实现

--- app/src/main/java/com/horse/proud/ui/login/RegisterActivity.kt
+++ app/src/main/java/com/horse/proud/ui/login/RegisterActivity.kt
@@ -15,11 +15,14 @@ import com.baidu.ocr.sdk.OnResultListener
 import com.baidu.ocr.sdk.exception.OCRError
 import com.baidu.ocr.sdk.model.AccessToken
 import com.baidu.ocr.ui.camera.CameraActivity
+import com.google.gson.Gson
 import com.horse.core.proud.Proud
+import com.horse.core.proud.extension.logError
 import com.horse.core.proud.extension.logWarn
 import com.horse.core.proud.extension.showToast
 import com.horse.core.proud.util.AndroidVersion
 import com.horse.proud.R
+import com.horse.proud.data.model.login.WordsResult
 import com.horse.proud.data.model.regist.Register
 import com.horse.proud.databinding.ActivityRegisterBinding
 import com.horse.proud.event.RegisterSucceedEvent
@@ -29,6 +32,7 @@ import com.horse.proud.util.FileUtil
 import com.horse.proud.util.RecognizeService
 import kotlinx.android.synthetic.main.activity_register.*
 import org.greenrobot.eventbus.EventBus
+import org.json.JSONObject
 import org.koin.android.ext.android.inject
 import pub.devrel.easypermissions.AfterPermissionGranted
 import pub.devrel.easypermissions.EasyPermissions
@@ -47,7 +51,7 @@ class RegisterActivity : BaseActivity() , EasyPermissions.PermissionCallbacks {
 
     val viewModel by lazy { ViewModelProviders.of(this, viewModelFactory).get(RegisterActivityViewModel::class.java) }
 
-    private lateinit var register:Register
+    private val register = Register()
 
     /**
      * 校园卡拍照验证的图片的临时地址
@@ -63,15 +67,34 @@ class RegisterActivity : BaseActivity() , EasyPermissions.PermissionCallbacks {
 
     override fun setupViews() {
         btn_verify.setOnClickListener {
-            //takePhoto()
-            initAccessToken()
+            if(et_name.text.toString().isEmpty()){
+                showToast("姓名不能为空")
+                return@setOnClickListener
+            }
+            if(et_number.text.toString().isEmpty()){
+                showToast("学号不能为空")
+                return@setOnClickListener
+            }
+            if(et_password.text.toString().isEmpty()||et_repassword.text.toString().isEmpty()){
+                showToast("密码不能为空")
+                return@setOnClickListener
+            }
+            if(et_password.text.toString() != et_repassword.text.toString()){
+                showToast("两次输入的密码不一致")
+                return@setOnClickListener
+            }
+            if(et_phone.text.toString().isEmpty()){
+                showToast("手机号码不能为空")
+                return@setOnClickListener
+            }
+            register.name = et_name.text.toString()
+            register.number = et_number.text.toString()
+            register.password = et_password.text.toString()
+            register.phoneNumber = et_phone.text.toString()
 
-
-//            register = Register()
-//            register.name = et_name.text.toString()
-//            register.number = et_number.text.toString()
-//            register.password = et_password.text.toString()
-//            viewModel.register(register)
+            if(checkPermission()){
+                initAccessToken()
+            }
         }
         btn_visit.setOnClickListener {
             MainActivity.actionStart(this)
@@ -82,16 +105,11 @@ class RegisterActivity : BaseActivity() , EasyPermissions.PermissionCallbacks {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         when (requestCode) {
-            TAKE_PHOTO -> if (resultCode == Activity.RESULT_OK) {
-                showToast("获取图片成功")
-                logWarn(TAG,photoUri.toString())
-                viewModel.getAccessToken(TYPE,AK,SK)
-            }
             REQUEST_CODE_HANDWRITING -> if(resultCode == Activity.RESULT_OK){
                 RecognizeService.recHandwriting(this, FileUtil.getSaveFile(applicationContext)!!.absolutePath,
                     object : RecognizeService.ServiceListener {
                         override fun onResult(result: String) {
-                            logWarn(TAG,result)
+                            regexCheck(result)
                         }
 
                     })
@@ -117,61 +135,81 @@ class RegisterActivity : BaseActivity() , EasyPermissions.PermissionCallbacks {
         OCR.getInstance(this).initAccessToken(object : OnResultListener<AccessToken> {
             override fun onResult(accessToken: AccessToken) {
                 val token = accessToken.accessToken
-                //hasGotToken = true
                 logWarn(TAG,token)
                 val intent = Intent(activity, CameraActivity::class.java)
-                intent.putExtra(
-                    CameraActivity.KEY_OUTPUT_FILE_PATH,
-                    FileUtil.getSaveFile(application)?.getAbsolutePath()
-                )
+                intent.putExtra(CameraActivity.KEY_OUTPUT_FILE_PATH, FileUtil.getSaveFile(application)?.absolutePath)
                 intent.putExtra(CameraActivity.KEY_CONTENT_TYPE, CameraActivity.CONTENT_TYPE_GENERAL)
                 startActivityForResult(intent, REQUEST_CODE_HANDWRITING)
             }
 
             override fun onError(error: OCRError) {
-                error.printStackTrace()
-                //alertText("licence方式获取token失败", error.message)
+                logError(TAG,error)
             }
         },Proud.getContext())
     }
 
     /**
-     * 打开摄像头拍照。
+     * 正则表达式验证身份
+     * */
+    private fun regexCheck(result:String){
+        //福州大学，匹配2次
+        var i1 = 2
+        //姓名，匹配1次
+        var i2 = 1
+        //学号，匹配1次
+        var i3 = 1
+        //福，匹配1次
+        var i4 = 1
+        val bean = Gson().fromJson(result,WordsResult::class.java)
+        for(item in bean.resultList){
+            with(item){
+                logWarn(TAG,"*******"+words)
+                if(words.matches(Regex(".*福州大学.*"))){
+                    i1 --
+                    logWarn(TAG,words)
+                }
+                if(words.matches(Regex(register.name))){
+                    i2 --
+                    logWarn(TAG,words)
+                }
+                if(words.matches(Regex(".*${register.number}"))){
+                    i3 --
+                    logWarn(TAG,words)
+                }
+                if(words.matches(Regex("福"))){
+                    i4 --
+                    logWarn(TAG,words)
+                }
+                if(words.matches(Regex("福.*"))){
+                    i4 --
+                    logWarn(TAG,words)
+                }
+            }
+        }
+        if(i1+i2+i3+i4 <= 0){
+            showToast("匹配成功")
+        }else{
+            showToast("匹配失败")
+        }
+    }
+
+    /**
+     * 检查权限
      */
     @AfterPermissionGranted(PRC_PHOTO_PICKER)
-    private fun takePhoto() {
+    private fun checkPermission():Boolean {
         val perms: Array<String> = arrayOf(
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.CAMERA)
-        if(EasyPermissions.hasPermissions(this, *perms)){
-            // 创建 File 对象，用于存储拍照后的图片
-            val outputImage = File(externalCacheDir, TEMP_PHOTO)
-            try {
-                if (outputImage.exists()) {
-                    outputImage.delete()
-                }
-                outputImage.createNewFile()
-            } catch (e: IOException) {
-                logWarn(TAG, e.message, e)
-            }
-
-            photoUri = if (AndroidVersion.hasNougat()) {
-                FileProvider.getUriForFile(this, "com.horse.fileprovider", outputImage)
-            } else {
-                Uri.fromFile(outputImage)
-            }
-            // 启动相机程序
-            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
-            startActivityForResult(intent, TAKE_PHOTO)
-        }else{
-            EasyPermissions.requestPermissions(
-                this,
+        if(!EasyPermissions.hasPermissions(this, *perms)){
+            EasyPermissions.requestPermissions(this,
                 "图片选择需要以下权限:\n\n1.访问设备上的照片\n\n2.拍照",
-                PRC_PHOTO_PICKER,
-                *perms
-            )
+                PRC_PHOTO_PICKER, *perms)
         }
+        if(!EasyPermissions.hasPermissions(this, *perms)){
+            return false
+        }
+        return true
     }
 
     //-------------------------------------EasyPermissions----------------------
@@ -201,47 +239,16 @@ class RegisterActivity : BaseActivity() , EasyPermissions.PermissionCallbacks {
 
         private const val TAG = "RegisterActivity"
 
-        const val TEMP_PHOTO = "taken_photo.jpg"
-
-        const val TAKE_PHOTO = 1000
-
+        /**
+         * 图片识别回调
+         * */
         const val REQUEST_CODE_HANDWRITING = 0
 
-        //申请相册权限回调标志
+        /**
+         * 申请相册权限回调标志
+         * */
         private const val PRC_PHOTO_PICKER: Int = 1
 
-        //识别校园卡用
-        const val SK = "hSimfEPbPMxvWMZ8RtXIGMqo7sBCrzoB"
-        const val AK = "auiGfbTHzngFsK1VrWI3c2ai"
-        const val TYPE = "client_credentials"
-        //const val token_url = "https://aip.baidubce.com/oauth/2.0/token?"
-
-        /**
-         * 获取识别校园卡用的token
-         * */
-//        private fun getAuth():String?{
-//            val getAccessTokenUrl = "${token_url}" +
-//                    "grant_type=client_credentials" +
-//                    "&client_id=${AK}" +
-//                    "&client_secret=${SK}"
-//
-//            try {
-//                val realUrl = URL(getAccessTokenUrl)
-//                val connection = realUrl.openConnection() as HttpURLConnection
-//                connection.requestMethod = "GET"
-//                connection.connect()
-//                val inReader = BufferedReader(InputStreamReader(connection.inputStream))
-//                var result = StringBuilder()
-//                inReader.forEachLine {
-//                    result.append(it)
-//                }
-//                val jsonObj = JSONObject(result.toString())
-//                return jsonObj.getString("access_token")
-//            }catch (e:Exception){
-//                logWarn(TAG,"token获取失败:${e.printStackTrace()}")
-//            }
-//            return null
-//        }
 
         fun actionStart(activity: Activity){
             val intent = Intent(activity,
